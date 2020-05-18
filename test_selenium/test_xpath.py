6bfd61b526572696430122b26eb6a98f960962e6
yuanpengxia <444233942@qq.com>
2020-05-18 21:46:59 +0800

    Commit test_xpath.py

--- test_selenium/test_xpath.py
+++ test_selenium/test_xpath.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -8,6 +9,8 @@ class TestWait:
     def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.baidu.com/")
+        #self.driver.implicitly_wait(3)
+        self.driver.maximize_window()
 
     def teardown(self):
         self.driver.quit()
