67bdd81feede13b6635752882b5f7888c5a9201f
yuanpengxia <444233942@qq.com>
2020-05-19 14:44:39 +0800

    Commit test_Action_Chains.py

--- test_selenium/test_ActionChains.py
+++ test_selenium/test_ActionChains.py
@@ -10,21 +10,28 @@ class TestActiongChains:
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.implicitly_wait(2)
+
     def teardown(self):
         self.driver.quit()
+
     def test_action_chains(self):
         self.driver.get("http://sahitest.com/demo/clicks.htm")
-        sleep(2)
         element_click = self.driver.find_element_by_xpath("/html/body/form/input[3]")
         element_doubleclick = self.driver.find_element_by_xpath("/html/body/form/input[2]")
-        element_rightclick = self.driver.find_element_by_xpath("/html/body/form/input[4]")
+        element_rightclick = self.driver.find_element_by_xpath("//input[@value='right click me']")
+        #element_rightclick = self.driver.find_element_by_xpath('/html/body/form/input[4]')
         action = ActionChains(self.driver)
-        action.click(element_click)
-        # sleep(3)
+        # action.click(element_click)
         # action.double_click(element_doubleclick)
-        # sleep(3)
-        # action.right_clich(element_rightclick)
-        sleep(3)
+        try:
+            action.click_and_hold(element_rightclick).release()
+            print("测试通过")
+        except:
+            print("测试未通过")
+
+        sleep(1)
         action.perform()
+
+
 if __name__ == '__main__':
     pytest.main()
